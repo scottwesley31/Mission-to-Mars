50a1a1a27b096434b89d9f5e9a39eb1f19147cb9
unknown <scottwesleybean@gmail.com>
2022-08-21 14:40:43 -0600

    Adding changes, debug Flask

--- scraping.py
+++ scraping.py
@@ -3,10 +3,31 @@ from splinter import Browser
 from bs4 import BeautifulSoup as soup
 from webdriver_manager.chrome import ChromeDriverManager
 import pandas as pd
+import datetime as dt
 
-# Set executable path and set up browser for scraping
-executable_path = {'executable_path': ChromeDriverManager().install()}
-browser = Browser('chrome', **executable_path, headless=False)
+# Define scrape_all function which will initialize a browser, create a data dictionary,
+# end the WebDriver, and return the scraped data.
+
+def scrape_all():
+    # Initialize headless driver for deployment
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    browser = Browser('chrome', **executable_path, headless=True)
+
+    # Set news title and paragraph variables; use mars_news function to pull news_title and news_paragraph
+    news_title, news_paragraph = mars_news(browser)
+
+    # Run all scraping functions and store results in dictionary
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "featured_image": featured_image(browser),
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return data
 
 # Define a news title and paragraph function
 def mars_news(browser):
@@ -27,14 +48,12 @@ def mars_news(browser):
     try:
         slide_elem = news_soup.select_one('div.list_text')
 
-        # Find the title information
-        slide_elem.find('div', class_='content_title')
-
         # Use the parent element to find the first 'a' tag and save it as 'news_title'
         news_title = slide_elem.find('div', class_='content_title').get_text()
 
         # Use the parent element to find the paragraph text
         news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+
     except AttributeError:
         return None, None
 
@@ -89,5 +108,8 @@ def mars_facts():
     # Convert df back into HTML code, add bootstrap
     return df.to_html()
 
-# Close the browser
-browser.quit()
+# Necessary Flask statement for it to run properly (also in app.py)
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
