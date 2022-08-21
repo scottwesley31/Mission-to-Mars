e87123d7b7bc880de811e3e0a3101f37596cc675
unknown <scottwesleybean@gmail.com>
2022-08-21 14:18:49 -0600

    Added functions to scraping.py

--- scraping.py
+++ scraping.py
@@ -8,61 +8,86 @@ import pandas as pd
 executable_path = {'executable_path': ChromeDriverManager().install()}
 browser = Browser('chrome', **executable_path, headless=False)
 
-# Assign the URL and instruct browser to visit it
-# Visit the mars nasa news site
-url = 'https://redplanetscience.com'
-browser.visit(url)
+# Define a news title and paragraph function
+def mars_news(browser):
 
-# Optional delay for loading the page
-browser.is_element_present_by_css('div.list_text', wait_time=1)
+    # Assign the URL and instruct browser to visit it
+    # Visit the mars nasa news site
+    url = 'https://redplanetscience.com'
+    browser.visit(url)
 
-# Set up HTML parser
-html = browser.html
-news_soup = soup(html, 'html.parser')
-slide_elem = news_soup.select_one('div.list_text')
+    # Optional delay for loading the page
+    browser.is_element_present_by_css('div.list_text', wait_time=1)
 
-# Find the title information
-slide_elem.find('div', class_='content_title')
+    # Set up HTML parser
+    html = browser.html
+    news_soup = soup(html, 'html.parser')
 
-# Use the parent element to find the first 'a' tag and save it as 'news_title'
-news_title = slide_elem.find('div', class_='content_title').get_text()
-news_title
+    # Add try/except for error handling
+    try:
+        slide_elem = news_soup.select_one('div.list_text')
 
-# Use the parent element to find the paragraph text
-news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
-news_p
+        # Find the title information
+        slide_elem.find('div', class_='content_title')
+
+        # Use the parent element to find the first 'a' tag and save it as 'news_title'
+        news_title = slide_elem.find('div', class_='content_title').get_text()
+
+        # Use the parent element to find the paragraph text
+        news_p = slide_elem.find('div', class_='article_teaser_body').get_text()
+    except AttributeError:
+        return None, None
+
+    return news_title, news_p
 
 # Featured Images
 
-# Set up the URL
-url = 'https://spaceimages-mars.com'
-browser.visit(url)
+# Define a featured image function
+def featured_image(browser):
 
-# Find and click the full image button
-full_image_elem = browser.find_by_tag('button')[1]
-full_image_elem.click()
+    # Set up the URL
+    url = 'https://spaceimages-mars.com'
+    browser.visit(url)
 
-# Parse the resulting html with soup
-html = browser.html
-img_soup = soup(html, 'html.parser')
+    # Find and click the full image button
+    full_image_elem = browser.find_by_tag('button')[1]
+    full_image_elem.click()
 
-# Find the relative image url
-img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
-img_url_rel
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = soup(html, 'html.parser')
 
-# Use the base URL to create an absolute URL
-img_url = f'https://spaceimages-mars.com/{img_url_rel}'
+    # Add try/except for error handling
+    try:
 
-# Create pandas DataFrame from Mars Facts table at https://galaxyfacts-mars.com
-df = pd.read_html('https://galaxyfacts-mars.com')[0]
-df.head()
+        # Find the relative image url
+        img_url_rel = img_soup.find('img', class_='fancybox-image').get('src')
+    
+    except AttributeError:
+        return None
 
-df.columns=['description', 'Mars', 'Earth']
-df.set_index('description', inplace=True)
-df
+    # Use the base URL to create an absolute URL
+    img_url = f'https://spaceimages-mars.com/{img_url_rel}'
 
-# Convert df back into HTML code
-df.to_html()
+    return img_url
+
+# Define the Mars Facts function
+def mars_facts():
+
+    # Add try/except handling
+    try:
+        # Create pandas DataFrame from Mars Facts table at https://galaxyfacts-mars.com
+        df = pd.read_html('https://galaxyfacts-mars.com')[0]
+    
+    except BaseException:
+        return None
+
+    # Assign columns and set index of dataframe
+    df.columns=['description', 'Mars', 'Earth']
+    df.set_index('description', inplace=True)
+
+    # Convert df back into HTML code, add bootstrap
+    return df.to_html()
 
 # Close the browser
 browser.quit()
