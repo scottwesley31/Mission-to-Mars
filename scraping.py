671a89383902d085a22e390a3a10e9fefa68406f
unknown <scottwesleybean@gmail.com>
2022-08-21 20:50:14 -0600

    scraping.py updated

--- scraping.py
+++ scraping.py
@@ -102,8 +102,8 @@ def mars_facts():
         return None
 
     # Assign columns and set index of dataframe
-    df.columns=['description', 'Mars', 'Earth']
-    df.set_index('description', inplace=True)
+    df.columns=['Description', 'Mars', 'Earth']
+    df.set_index('Description', inplace=True)
 
     # Convert df back into HTML code, add bootstrap
     return df.to_html()
